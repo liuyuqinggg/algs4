353aed92ef5740c2d0289bc6ec7a42e712e7bc60
liuyuqinggg <lyq1464972196@gmail.com>
2020-04-25 13:01:37 +0800

    算法第四版练习题

---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+
+    }
+}
---
+++ src/utils/RandomStringGenerator.java
@@ -0,0 +1,42 @@
+package utils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class RandomStringGenerator {
+    public static void main(String[] args) throws IOException {
+        File file = new File("F:\\IDEA\\algs4\\src\\strings.txt");
+
+        if(file.exists()){
+            file.delete();
+        }
+
+        if(!file.exists()){
+            file.createNewFile();
+        }
+        FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(fileWriter);
+        for (int i = 0; i < 50000 ; i++) {
+            String str = getRandomString(8);
+            bw.write(str + " ");
+//            bw.newLine();
+        }
+
+        bw.close();
+        System.out.println("finish");
+    }
+
+    public static String getRandomString(int length){
+        String str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random=new Random();
+        StringBuffer sb=new StringBuffer();
+        for(int i=0;i<length;i++){
+            int number=random.nextInt(62);
+            sb.append(str.charAt(number));
+        }
+        return sb.toString();
+    }
+}
